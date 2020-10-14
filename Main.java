3dfc45d867fd312a6c472ce0862089540d16d878
Budad9807 <Budad9807@wrdsb.ca>
2020-10-14 15:57:32 +0000

    Made a program that calculates the pizza size for however big the user puts in

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * The cost of the pizza by its size
+ * @author David
  */
 public class Main {
 
@@ -9,7 +10,40 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+  //they are the stronge for amounts of money
+  double LAB = 0.75;
+  double POWER = 0.99;
+  double DIA = 0.50;
+  double HST = 0.13;
+
+    // make a scannerfor user input
+    Scanner input = new Scanner(System.in);
+    // asking the user for pizza size
+    System.out.println("What is the size of the pizza?");
+    // stating variable
+    int size = input.nextInt();
+    double subtotal,taxs,total;
+    // make the calculation the pizza
+    subtotal = (DIA * size) + LAB + POWER;
+    taxs = subtotal * HST;
+    total = taxs + subtotal;
+    // print out to the screen the subtotal,taxs,total
+    System.out.println("Subtotal: $" + subtotal);
+    System.out.println("taxs: $" + taxs);
+    System.out.println("total: $" + total);
+    // make a statement on the size is smaller then 20
+    if(size >= 1 && size <= 20){
+      System.out.println("We are going to make you a cute little pizza");
+    // make a statement on the size being greater than 20
+    }else if(size > 20 && size < 40){
+      System.out.println("This will be delicious!");
+    // make a statement on the size being greater than 40
+    }else if( size > 40){
+      System.out.println("Whoa, big pizza! You might need a truck to get this home");
+    }
+    
+
+    
     
   }
 }
